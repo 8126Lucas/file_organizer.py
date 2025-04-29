1124d03550fe556acc90d2af0d9d762743896ae2
lucas <lucaspereirasantos8126@gmail.com>
2025-04-29 09:29:01 +0100

    Modified createDir.py to be compatible with Linux, Windows and MacOS

--- createDir.py
+++ createDir.py
@@ -3,26 +3,26 @@ from pathlib import Path
 
 
 def createDir(currentDir):
-    if not os.path.exists(currentDir + "/DOCUMENTS"):
-        os.makedirs(currentDir + "/DOCUMENTS")
-    if not os.path.exists(currentDir + "/LaTeX"):
-        os.makedirs(currentDir + "/LaTeX")
-    if not os.path.exists(currentDir + "/IMAGES"):
-        os.makedirs(currentDir + "/IMAGES")
-    if not os.path.exists(currentDir + "/VIDEOS"):
-        os.makedirs(currentDir + "/VIDEOS")
-    if not os.path.exists(currentDir + "/GOOGLE"):
-        os.makedirs(currentDir + "/GOOGLE")
-    if not os.path.exists(currentDir + "/PDF"):
-        os.makedirs(currentDir + "/PDF")
-    if not os.path.exists(currentDir + "/AUDIO"):
-        os.makedirs(currentDir + "/AUDIO")
-    if not os.path.exists(currentDir + "/COMPRESSED FILES"):
-        os.makedirs(currentDir + "/COMPRESSED FILES")
-    if not os.path.exists(currentDir + "/EXECUTABLES"):
-        os.makedirs(currentDir + "/EXECUTABLES")
-    if not os.path.exists(currentDir + "/PROGRAMMING"):
-        os.makedirs(currentDir + "/PROGRAMMING")
+    if not os.path.exists(os.path.join(currentDir, "DOCUMENTS")):
+        os.makedirs(os.path.join(currentDir, "DOCUMENTS"))
+    if not os.path.exists(os.path.join(currentDir, "LaTeX")):
+        os.makedirs(os.path.join(currentDir, "LaTeX"))
+    if not os.path.exists(os.path.join(currentDir, "IMAGES")):
+        os.makedirs(os.path.join(currentDir, "IMAGES"))
+    if not os.path.exists(os.path.join(currentDir, "VIDEOS")):
+        os.makedirs(os.path.join(currentDir, "VIDEOS"))
+    if not os.path.exists(os.path.join(currentDir, "GOOGLE")):
+        os.makedirs(os.path.join(currentDir, "GOOGLE"))
+    if not os.path.exists(os.path.join(currentDir, "PDF")):
+        os.makedirs(os.path.join(currentDir, "PDF"))
+    if not os.path.exists(os.path.join(currentDir, "AUDIO")):
+        os.makedirs(os.path.join(currentDir, "AUDIO"))
+    if not os.path.exists(os.path.join(currentDir, "COMPRESSED FILES")):
+        os.makedirs(os.path.join(currentDir, "COMPRESSED FILES"))
+    if not os.path.exists(os.path.join(currentDir, "EXECUTABLES")):
+        os.makedirs(os.path.join(currentDir, "EXECUTABLES"))
+    if not os.path.exists(os.path.join(currentDir, "PROGRAMMING")):
+        os.makedirs(os.path.join(currentDir, "PROGRAMMING"))
 
 
 
